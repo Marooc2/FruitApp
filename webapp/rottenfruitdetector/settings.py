c6164ccd72675a1adf6f874374fef68f0b98dc27
Mauricio <u201510971@upc.edu.pe>
2020-10-28 14:38:42 -0500

    commit2

--- webapp/rottenfruitdetector/settings.py
+++ webapp/rottenfruitdetector/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'soqpalpf+ozyms12k27(@jg2nn!86vcscklq7lnr_j5=8hb$9h'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
